418397d49bb9558abe90d2d5e49657b29a3f6ee5
Chris <csjohnson79@icloud.com>
2020-01-28 20:38:10 -0700

    Updates

---
+++ precache-manifest.6b8b26fc832deb9ef6c5d3af4a059f95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9573bf734135b4ca5f79cc79745249c1",
+    "url": "/ecommerce-store/index.html"
+  },
+  {
+    "revision": "38101eb4ffeae3abbfc3",
+    "url": "/ecommerce-store/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "8fe9210ae6609bce6f7c",
+    "url": "/ecommerce-store/static/js/2.d661c50c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ecommerce-store/static/js/2.d661c50c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38101eb4ffeae3abbfc3",
+    "url": "/ecommerce-store/static/js/main.347ef158.chunk.js"
+  },
+  {
+    "revision": "8440fd44208883800448",
+    "url": "/ecommerce-store/static/js/runtime-main.ea170280.js"
+  }
+]);
