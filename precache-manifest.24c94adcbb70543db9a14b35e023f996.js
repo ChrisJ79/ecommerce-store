ddc8cd6a821531fdf422545021b53d934c6cde6b
Chris <csjohnson79@icloud.com>
2020-02-07 11:48:21 -0700

    Updates

--- precache-manifest.24c94adcbb70543db9a14b35e023f996.js
+++ precache-manifest.24c94adcbb70543db9a14b35e023f996.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "987574555e5ad5c60ef03279c552e9d3",
+    "revision": "7bf7ebe75da8112be3ede0d0082ea1b3",
     "url": "/ecommerce-store/index.html"
   },
   {
-    "revision": "8422143ec2a598fdc9e4",
+    "revision": "aa770812a448d681e200",
     "url": "/ecommerce-store/static/css/main.7d771e04.chunk.css"
   },
   {
-    "revision": "6840bfb6976a2c9efd7c",
-    "url": "/ecommerce-store/static/js/2.ec23d479.chunk.js"
+    "revision": "c13ede9b4f60c88e670a",
+    "url": "/ecommerce-store/static/js/2.14294270.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/ecommerce-store/static/js/2.ec23d479.chunk.js.LICENSE"
+    "url": "/ecommerce-store/static/js/2.14294270.chunk.js.LICENSE"
   },
   {
-    "revision": "8422143ec2a598fdc9e4",
-    "url": "/ecommerce-store/static/js/main.68c01816.chunk.js"
+    "revision": "aa770812a448d681e200",
+    "url": "/ecommerce-store/static/js/main.5a24f383.chunk.js"
   },
   {
     "revision": "8440fd44208883800448",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ecommerce-store/precache-manifest.902b4fb67afe1e3e819b4221d036d9ec.js"
+  "/ecommerce-store/precache-manifest.24c94adcbb70543db9a14b35e023f996.js"
 );
 
 self.addEventListener('message', (event) => {
