c54e134058a44c1736a83ac38ad821e82e6e20af
Chris <csjohnson79@icloud.com>
2020-02-07 15:19:30 -0700

    other changes in attempt to get Shop page to render

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shoppage';
 
@@ -13,10 +13,12 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
-      <switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-      </switch>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
