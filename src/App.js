498cd82cbb997117f4b4b6b1dc2aa8c440492fed
Chris <csjohnson79@icloud.com>
2020-02-13 19:43:02 -0700

    set currentUser state in app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import { BrowserRouter, HashRouter, Link, Switch, Router, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shoppage';
 import SignInAndRegister from './pages/sign-in-and-register/sign-in-and-register';
 import Header from './components/header/header';
+import {auth} from './firebase/firebase.utils';
 
 
-function App() {
-  return (
-    <div>
-      <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndRegister} />
-        </Switch>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            currentUser: null
+        }
+    }
+    componentDidMount() {
+        auth.onAuthStateChanged(user => {
+            this.setState({currentUser: user});
+            console.log(user);
+        });
+    }
+    render() {
+      return (
+        <div>
+          <Header />
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route path="/shop" component={ShopPage} />
+              <Route path="/signin" component={SignInAndRegister} />
+            </Switch>
+        </div>
+      );
+    }
 }
 
 
