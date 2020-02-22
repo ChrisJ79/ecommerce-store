7c8d9063974d9d34fdd28f75093899466476d702
Chris <csjohnson79@icloud.com>
2020-02-21 19:05:28 -0700

    configured firebase, firebase.utils.js, and app.js to query data from new users that sign in with google and store in firebase database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shoppage';
 import SignInAndRegister from './pages/sign-in-and-register/sign-in-and-register';
 import Header from './components/header/header';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 
 class App extends Component {
@@ -19,10 +19,24 @@ class App extends Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        auth.onAuthStateChanged(user => {
-            this.setState({currentUser: user});
-            console.log(user);
-        });
+      this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+        if (userAuth) {
+          const userRef = await createUserProfileDocument(userAuth);
+
+          userRef.onSnapshot(snapShot => {
+            this.setState({
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            });
+
+            console.log(this.state);
+          });
+        }
+
+        this.setState({ currentUser: userAuth });
+      });
     }
 
     componentWillUnmount() {
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,6 +14,27 @@ const firebaseConfig = {
     measurementId: "G-3EYB0EHMMR"
   };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+    // console.log(firestore.doc(''));
+    const userRef = firestore.doc('users/${userAuth.uid}');
+    const snapShot = await userRef.get();
+    if (!snapShot.exists) {
+      const {displayName, email} = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({
+          displayName, email, createdAt, ...additionalData
+        })
+      } catch (error) {
+        console.log('error creating user', error.message);
+      }
+    }
+    return userRef;
+    }
+
+
 firebase.initializeApp(firebaseConfig);
 console.log(firebase.app().name);
 
