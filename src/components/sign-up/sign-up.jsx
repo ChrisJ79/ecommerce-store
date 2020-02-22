5bc4f217831345695067462566eaad50db26a5f0
Chris <csjohnson79@icloud.com>
2020-02-21 22:16:02 -0700

    created SignUp component

---
+++ src/components/sign-up/sign-up.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import CustomButton from '../custom-button/custom-button';
+import FormInput from '../form-input/form-input';
+import {} from '../../firebase/firebase.utils.js';
+import './sign-up.scss';
+
+class SignUp extends Component {
+    render() {
+        return(
+
+        )
+    }
+}
+
+export default SignUp;
