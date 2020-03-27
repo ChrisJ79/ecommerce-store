2e802768fa90b03eb855a724517253f4fc33aca9
Chris <csjohnson79@icloud.com>
2020-03-27 16:31:54 -0600

    imported connect into app.js, erased constructor in app component, exported connect component with null and mapDispatchToProps as the arguments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import { HashRouter, Link, Switch, Router, Route } from 'react-router-dom';
+import { connect } from "react-redux";
+import { setCurrentUser } from './redux/user/user.actions'; 
+
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shoppage';
 import SignInAndRegister from './pages/sign-in-and-register/sign-in-and-register';
@@ -9,33 +12,24 @@ import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            currentUser: null
-        }
-    }
-
+  
     unsubscribeFromAuth = null;
 
     componentDidMount() {
+      const { setCurrentUser } = this.props;
       this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
         if (userAuth) {
           const userRef = await createUserProfileDocument(userAuth);
 
           userRef.onSnapshot(snapShot => {
-            this.setState({
-              currentUser: {
+            setCurrentUser({
                 id: snapShot.id,
                 ...snapShot.data()
-              }
             });
-
-            console.log(this.state);
           });
         }
 
-        this.setState({ currentUser: userAuth });
+        setCurrentUser(userAuth);
       });
     }
 
@@ -57,4 +51,8 @@ class App extends Component {
 }
 
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
